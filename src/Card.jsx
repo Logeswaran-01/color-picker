a5fad32728fbdac46883d0394697fb27c9e00d1e
Logeswaran-01 <balajayanthi9500@gmail.com>
2025-06-20 13:01:44 +0530

    Initial commit for card project

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,17 +1,25 @@
-import profile from './assets/profile.jpg'
+import React, {useState} from 'react'
 function Card(){
+    const [ color , setColor ] = useState("#ffffff")
+    const updateColor = (e) => setColor(e.target.value)
     return(
-        <>
-          <div className="whole-card">
-              <h1 className="class">CARD</h1>
-              <img className="picture" src={profile} alt="PROFILE_PIC" />
-              <h2 className="name">LOGESWARAN B</h2>
-              <p className="about-me">I am passionate to work in IT field as a WEB DEVELOPER</p>
+        <div className="color-pic">
+            <div className="color">
+                <h1>COLOR PICKER</h1>
+                <div className="color-picker" >
+                <p>SELECTED COLOR : {color}</p>
+                </div>
+                <label>SELECT A COLOR</label>
+                <input type="color" value={color} onChange={updateColor} />
 
-          </div>
-           
-        </>
-        
-    );
+
+            </div>
+         </div>
+
+
+
+    )
+
+
 }
 export default Card
